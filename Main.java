3351980e71ef8fa122fdcb9d101b94b7126f63fe
Kailash Nath Yadav <107490624+KailashNathYadav@users.noreply.github.com>
2023-01-25 15:58:54 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.driver;
+import com.service.Construction;
+
+public class Main {
+	public static void main(String[] args) {
+		Construction construction = new Construction();
+		construction.getData();
+		construction.assembledFloor();
+	}
+}
